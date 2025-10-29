aac6719c2db0faf1b8b60c966a87d65136be29ae
Ankit Singh <142376440+chauhanAni5959@users.noreply.github.com>
2025-10-29 10:43:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-//your JS code here. If required.
+const input = document.getElementById('ip');
+const button = document.getElementById('btn');
+const output = document.getElementById('output');
+
+function firstPromise(number) {
+	return new Promise(resolve =>{
+		setTimeout(()=>{
+			output.textContent = `Result: ${number}`;
+			resolve(number);
+			
+		},2000);
+	});
+	
+}
+
+function secondPromise(number) {
+	return new Promise(resolve =>{
+		setTimeout(() =>{
+			const result = number * 2;
+			output.textContent = `Result: ${result}`;
+			resolve(result);
+		},2000);
+	});
+	
+}
+function thirdPromise(number) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const result = number - 3;
+      output.textContent = `Result: ${result}`;
+      resolve(result);
+    }, 1000);
+  });
+}
+
+function fourthPromise(number) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const result = number / 2;
+      output.textContent = `Result: ${result}`;
+      resolve(result);
+    }, 1000);
+  });
+}
+
+function fifthPromise(number) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const result = number + 10;
+      output.textContent = `Final Result: ${result}`;
+      resolve(result);
+    }, 1000);
+  });
+}
+
+button.onclick = function () {
+	const num = Number(input.value);
+	output.textContent = '';
+
+	firstPromise(num)
+		.then(secondPromise)
+		.then(thirdPromise)
+	    .then(fourthPromise)
+		.then(fifthPromise);
+	
+};
+
